8ff7d53e85133c66e4862ecad3b55a1ffad78069
J. Schuster <joe.schust@gmail.com>
2017-09-08 23:47:35 -0600

    Removed useless files, finished ga's population

--- Implementation/GADNLM/src/main/java/com/parma/genetics/Population.java
+++ Implementation/GADNLM/src/main/java/com/parma/genetics/Population.java
@@ -1,7 +1,10 @@
 package com.parma.genetics;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import com.parma.genetics.settings.GaSettings;
 
 public class Population {
 
@@ -12,15 +15,41 @@ public class Population {
 	
 	private final Random random = new Random();
 	
-	public Population() {
-		
+	private final GaSettings settings;
+	
+	public Population(GaSettings settings) {
+		this.population = new ArrayList<ParamIndividual>();
+		this.safebox = new ArrayList<ParamIndividual>();
+		this.settings = settings;
 	}
 	
 	public void initializePopulation(int maxInd) {
 		for (int i = 0; i < maxInd; i++) {
 			ParamIndividual p = new ParamIndividual();
 			
+			p.setLambda(getRandomFloatBetween(settings.getLowerLambda(), 
+					settings.getUpperLambda()));
+			
+			p.setSigma_r(getRandomIntegerBetween(settings.getLowerSigmaR(), 
+					settings.getUpperSigmaR()));
+			
+			p.setSigma_s(getRandomIntegerBetween(settings.getLowerSigmaS(), 
+					settings.getUpperSigmaS()));
+			
+			population.add(p);
 		}
+	}
+	
+	public void addBestToSafebox() {
+		safebox.add(population.get(0));
+	}
+	
+	private int getRandomIntegerBetween(int lower, int upper) {
+		return random.nextInt(upper - lower + 1) + lower;
+	}
+	
+	private float getRandomFloatBetween(float lower, float upper) {
+		return random.nextFloat() * (upper - lower) + lower;
 	}
 	
 }
--- Implementation/SPACE/src/main/java/tec/psa/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package tec.psa.repository;
-
-import tec.psa.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
