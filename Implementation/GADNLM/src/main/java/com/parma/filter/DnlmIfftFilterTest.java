a1d7a26f74f0c5f71d486c2e9bab6b7c1a652b31
J. Schuster <joe.schust@gmail.com>
2017-09-24 11:37:47 -0600

    Fix filter test

--- Implementation/GADNLM/src/main/java/com/parma/filter/DnlmIfftFilterTest.java
+++ Implementation/GADNLM/src/main/java/com/parma/filter/DnlmIfftFilterTest.java
@@ -9,21 +9,18 @@ public class DnlmIfftFilterTest {
   @Test
   public void test() {
     ImageHandler ih = new ImageHandler();
-<<<<<<< HEAD
     Mat image = ih.leerImagenGrises("C:\\Users\\Joel\\Desktop\\FastDnlmv2\\lena.jpg");
-=======
     Mat matriz = ih.leerImagenGrises("C:\\Users\\Eliot\\Desktop\\FastDnlmv2\\lena.jpg");
->>>>>>> 4e1ecc73e807cbcdb6702a6efdda000c9488f5f9
 
     double w = 21;
     double w_n = 3;
     double sigma_r = 100;
 
-<<<<<<< HEAD
     long temp = System.currentTimeMillis();
     
     DnlmIfftFilter myFilter = new DnlmIfftFilter(image, w, w_n, sigma_r);
     myFilter.filterImage();
+    
     Mat resultado = myFilter.getResult();
     double checkSum = 0;
     
@@ -34,22 +31,7 @@ public class DnlmIfftFilterTest {
     }
     
     System.out.println(checkSum + "");
-    
-    Mat nueva = new Mat();
-    image.copyTo(nueva);
-   
-    long end = System.currentTimeMillis();   
-    
-    System.out.println(end - temp);
-=======
-    
-    long temp = System.currentTimeMillis();
-
-    
-    DnlmIfftFilter myFilter = new DnlmIfftFilter(matriz, w, w_n, sigma_r);
-    myFilter.filterImage();
->>>>>>> 4e1ecc73e807cbcdb6702a6efdda000c9488f5f9
-    
+       
     //assertTrue("Mismatch.", );
   }
 
