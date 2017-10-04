53e900d6b2ce0beb3a86cc77b6a754e87ef0e71c
J. Schuster <joe.schust@gmail.com>
2017-10-04 13:33:03 -0600

    Fixed meshgrid

--- Implementation/GADNLM/src/main/java/com/parma/filter/DnlmFilter.java
+++ Implementation/GADNLM/src/main/java/com/parma/filter/DnlmFilter.java
@@ -12,15 +12,16 @@ public class DnlmFilter {
 
   public Mat filter(Mat I, double w, double w_n, double sigma_r) {
 
-    Mat G = new Mat();
-    I.convertTo(G, CvType.CV_64F);
+    Mat G = new Mat(I.size(), CvType.CV_64FC1);           
+    I.copyTo(G);
+    G.convertTo(G, CvType.CV_64FC1);
 
-    int size_x = I.rows();
-    int size_y = I.cols();
+    int size_x = G.rows();
+    int size_y = G.cols();
 
-    Mat R = Mat.zeros(size_x, size_y, CvType.CV_64F);
+    Mat R = Mat.zeros(size_x, size_y, CvType.CV_64FC1);
 
-    Mat II2 = new Mat();
+    Mat II2 = new Mat(new Size(size_x, size_y), CvType.CV_64FC1);
     Imgproc.integral(II2, G.mul(G));
 
     double sigma_s = w / 1.5;
@@ -28,23 +29,24 @@ public class DnlmFilter {
     Range r = new Range(new double[] {-w, w});
     meshgrid(r, r, X, Y);
 
-    X.mul(X);
-    Y.mul(Y);
-    Mat S = new Mat();
-    S.push_back(X);
-    S.push_back(Y);
-    Core.multiply(S, new Scalar(-1.0 / (2.0 * Math.pow(sigma_s, 2))), S);
-
-    Mat GaussW = new Mat(S.width(), S.height(), 1);
-
+    X = X.mul(X);
+    Y = Y.mul(Y);    
+    Mat S = new Mat(X.size(), CvType.CV_64FC1);
+    
+    Core.add(X, Y, S);      
+    Core.multiply(S, new Scalar(-1.0 / (2.0 * sigma_s * sigma_s)), S);
+    
+    Mat GaussW = new Mat(S.size(), CvType.CV_64FC1);
     for (int i = 0; i < GaussW.width(); i++) {
       for (int j = 0; j < GaussW.height(); j++) {
-        double pixel = Math.pow(Math.E, GaussW.get(i, j)[0]);
+        double pixel = Math.pow(Math.E, S.get(i, j)[0]);
         GaussW.put(i, j, pixel);
       }
     }
 
     Mat U = NoAdaptativeUSM(G, 3, 17, 0.005);
+    
+    System.out.println(U.dump());
 
     for (int i = 0; i < size_x; i++) {
       for (int j = 0; j < size_y; j++) {
@@ -57,10 +59,14 @@ public class DnlmFilter {
           int jMax = (int) Math.min(j + w + w_n, size_y);
           // get current window
           I = G.submat(iMin, iMax, jMin, jMax);
+          I.convertTo(I, CvType.CV_64FC1);
+          
+          System.out.println(I.get(13, 13)[0]);
+          
           int sizeW_x = I.width();
           int sizeW_y = I.height();
           // create output matrix
-          Mat O = Mat.zeros((int) (sizeW_x - 2 * w_n), (int) (sizeW_y - 2 * w_n), CvType.CV_64F);
+          Mat O = Mat.zeros((int) (sizeW_x - 2 * w_n), (int) (sizeW_y - 2 * w_n), CvType.CV_64FC1);
 
           // extract pixel neighborhood P local region
           int mMin_p = (int) (i - w_n);
@@ -74,6 +80,7 @@ public class DnlmFilter {
 
           // get current neighborhood p
           Mat neighbor_p = G.submat(mMin_p, mMax_p, nMin_p, nMax_p);
+          neighbor_p.convertTo(neighbor_p, CvType.CV_64FC1);
           int sizeP_x = neighbor_p.width();
           int sizeP_y = neighbor_p.height();
 
@@ -82,16 +89,16 @@ public class DnlmFilter {
           int mm = sizeW_x + sizeP_x - 1;
           int nn = sizeW_y + sizeP_y - 1;
           // pad, multiply and transform back
-          Mat C_a = Mat.zeros(new Size(mm, nn), CvType.CV_64F);
-          Mat C_b = Mat.zeros(new Size(mm, nn), CvType.CV_64F);
+          Mat C_a = Mat.zeros(new Size(mm, nn), CvType.CV_64FC1);
+          Mat C_b = Mat.zeros(new Size(mm, nn), CvType.CV_64FC1);
           Core.copyMakeBorder(I, I, 0, nn - sizeW_y, 0, mm - sizeW_x, Core.BORDER_CONSTANT);
           Core.dft(I, C_a);
           Core.copyMakeBorder(neighbor_p, neighbor_p, 0, nn - sizeP_y, 0, mm - sizeP_x,
               Core.BORDER_CONSTANT);
           Core.dft(neighbor_p, C_b);
           Core.flip(C_b, C_b, -1); // rot90 two times
-          C_b.mul(C_a);
-          Mat C = new Mat(new Size(mm, nn), CvType.CV_64F);
+          C_b = C_b.mul(C_a);
+          Mat C = new Mat(new Size(mm, nn), CvType.CV_64FC1);
           Core.dft(C_b, C, Core.DFT_INVERSE, 0);
           // padding constants (for output of size == size(A))
           int padC_m = (int) Math.ceil((sizeP_x - 1) / 2.0);
@@ -118,10 +125,10 @@ public class DnlmFilter {
               O.put(k0, k1, pixel);
             }
           }
-          O.mul(GaussW);
+          O = O.mul(GaussW);
           double norm_factor = Core.sumElems(O).val[0];          
           Mat OxU_sub = U.submat((int)(iMin + w_n), (int)(iMax - w_n), (int)(jMin + w_n), (int)(jMax - w_n));
-          OxU_sub.mul(O);
+          OxU_sub = OxU_sub.mul(O);
           Core.multiply(OxU_sub, new Scalar(1.0 / norm_factor), OxU_sub);
           R.put(i, j, Core.sumElems(OxU_sub).val[0]);
         }
@@ -134,13 +141,13 @@ public class DnlmFilter {
    * No adaptative laplacian
    */
   private Mat NoAdaptativeUSM(Mat SrcImage, double lambda, double kernelSize, double kernelSigma) {
-    Mat kernel = new Mat(17, 17, 1);
+    Mat kernel = new Mat(17, 17, CvType.CV_64FC1);
     for (int i = 0; i < kernel.width(); i++) {
       for (int j = 0; j < kernel.height(); j++) {
         kernel.put(i, j, -276.8166);
       }
     }
-    Mat Z = new Mat();
+    Mat Z = new Mat(SrcImage.size(), CvType.CV_64FC1);
     Imgproc.filter2D(Z, SrcImage, 1, kernel);
     double maxZ = 0, maxSrc = 0;
     // get max (abs) of Z
@@ -160,27 +167,45 @@ public class DnlmFilter {
       }
     }
     Core.multiply(Z, new Scalar(lambda * maxSrc / maxZ), Z);
-    Mat U = new Mat();
-    Core.add(SrcImage, Z, U);
+    Mat U = new Mat(SrcImage.size(), CvType.CV_64FC1);
+    Core.add(SrcImage, Z, U, Mat.ones(SrcImage.size(), CvType.CV_8UC1), CvType.CV_64FC1);
     return U;
   } // end NoAdaptativeUSM
 
   private void meshgrid(Range xgv, Range ygv, Mat X, Mat Y) {
-    int[] t_x = new int[xgv.size()];
-    int[] t_y = new int[ygv.size()];
+    double[] t_x = new double[xgv.size() + 1];
+    double[] t_y = new double[ygv.size() + 1];
     for (int i = xgv.start; i <= xgv.end; i++) {
-      t_x[i] = i;
+      int index = i - xgv.start;
+      t_x[index] = i;
     }
     for (int i = ygv.start; i <= ygv.end; i++) {
-      t_y[i] = i;
+      int index = i - ygv.start;
+      t_y[index] = i;
     }
-    Mat xgvM = new Mat();
-    xgvM.put(1, 0, t_x);
-    Mat ygvM = new Mat();
-    ygvM.put(1, 0, t_y);
-    Core.repeat(xgvM.reshape(1, 1), (int) ygvM.total(), 1, X);
-    Core.repeat(xgvM.reshape(1, 1).t(), 1, (int) xgvM.total(), Y);
+    Mat xgvM = new Mat(new Size(1, t_x.length), CvType.CV_64FC1);
+    xgvM.put(0, 0, t_x);
+    Mat ygvM = new Mat(new Size(1, t_y.length), CvType.CV_64FC1);
+    ygvM.put(0, 0, t_y);
+    Core.repeat(xgvM.reshape(1, 1), (int) ygvM.total(), 1, X);       
+    Core.repeat(ygvM.reshape(1, 1).t(), 1, (int) xgvM.total(), Y);
   } // end Meshgrid
 
-}
+} /* end DnlmFilter */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Implementation/GADNLM/src/main/java/com/parma/filter/DnlmFilterTest.java
@@ -0,0 +1,25 @@
+package com.parma.filter;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import com.parma.images.ImageHandler;
+
+public class DnlmFilterTest {
+
+  @Test
+  public void test() {
+    
+    ImageHandler ih = new ImageHandler();
+    Mat imagen = ih.leerImagenGrises("C:\\Users\\Joel\\Desktop\\TEMP\\lena.jpg");
+    
+    DnlmFilter filter = new DnlmFilter();
+    
+    Mat res = filter.filter(imagen, 3, 4, 5);
+    
+    System.out.println("Checksum: " + Core.sumElems(res).val[0]);
+    
+  }
+
+}
