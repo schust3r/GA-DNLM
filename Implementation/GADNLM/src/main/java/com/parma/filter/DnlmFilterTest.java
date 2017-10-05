4ae6adb92cea962ad848c0d98c79972a55b09ef1
J. Schuster <joe.schust@gmail.com>
2017-10-05 17:59:25 -0600

    Finished DNLM-IDFT filter and unit test

--- Implementation/GADNLM/src/main/java/com/parma/filter/DnlmFilterTest.java
+++ Implementation/GADNLM/src/main/java/com/parma/filter/DnlmFilterTest.java
@@ -1,44 +1,35 @@
 package com.parma.filter;
 
 import static org.junit.Assert.*;
-
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
 import org.junit.Test;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import com.parma.images.ImageHandler;
-import com.parma.segmentation.Otsu;
-import com.parma.segmentation.Thresholding;
 
 public class DnlmFilterTest {
 
   @Test
-  public void test(){
-    
+  public void test() {
+
     ImageHandler ih = new ImageHandler();
-    
-    String filename= "coins.tif";
-    
-    Mat imagen = ih.leerImagenGrises("test_files/input/"+filename);
+
+    String filename = "coins.jpg";
+
+    Mat imagen = ih.leerImagenGrises("test_files/input/" + filename);
     DnlmFilter filter = new DnlmFilter();
     double mils = System.currentTimeMillis();
-    
-  
-	
-	Mat res = null;
-	res = filter.filter(imagen, 3, 3, 5);
 
-	
-    System.out.println("Time of processing: "+( System.currentTimeMillis()-mils));
-    ih.guardarImagen("C:/Users/Eliot/Desktop", filename+"_out", "png", res);
+    Mat res = null;
+    res = filter.filter(imagen, 3, 3, 5);
+
+    System.out.println("Time of processing: " + (System.currentTimeMillis() - mils));
+    ih.guardarImagen("test_files/output/", filename + "_out", "jpg", res);
     
-    
-    
-   
-    System.out.println("Checksum: " + Core.sumElems(res).val[0]);        
-    
+    double checksum = Core.sumElems(res).val[0];
+    System.out.printf("Checksum: %f\n", checksum);
+
+    assertTrue("Resultado inaceptable.", checksum - 10827950.3390755 < 1750);
+
   }
 
 }
