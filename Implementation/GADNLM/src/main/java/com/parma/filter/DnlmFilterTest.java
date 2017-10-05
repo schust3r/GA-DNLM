a04f79297c7804d0231fc1b3a898b3175e5e4e8e
jubileus95 <therionbg.95@gmail.com>
2017-10-05 17:15:43 -0600

    Parallelized filter

--- Implementation/GADNLM/src/main/java/com/parma/filter/DnlmFilterTest.java
+++ Implementation/GADNLM/src/main/java/com/parma/filter/DnlmFilterTest.java
@@ -1,24 +1,42 @@
 package com.parma.filter;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import org.junit.Test;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import com.parma.images.ImageHandler;
+import com.parma.segmentation.Otsu;
+import com.parma.segmentation.Thresholding;
 
 public class DnlmFilterTest {
 
   @Test
-  public void test() {
+  public void test(){
     
     ImageHandler ih = new ImageHandler();
-    Mat imagen = ih.leerImagenGrises("test_files/input/lena.jpg");
     
+    String filename= "coins.tif";
+    
+    Mat imagen = ih.leerImagenGrises("test_files/input/"+filename);
     DnlmFilter filter = new DnlmFilter();
+    double mils = System.currentTimeMillis();
     
-    Mat res = filter.filter(imagen, 3, 3, 5);
-    ih.guardarImagen("C:/Users/Eliot/Desktop", "2", "png", res);
+  
+	
+	Mat res = null;
+	res = filter.filter(imagen, 3, 3, 5);
+
+	
+    System.out.println("Time of processing: "+( System.currentTimeMillis()-mils));
+    ih.guardarImagen("C:/Users/Eliot/Desktop", filename+"_out", "png", res);
     
+    
+    
+   
     System.out.println("Checksum: " + Core.sumElems(res).val[0]);        
     
   }
